f942d765b451ea308fdcd8e3da3b1993160a0d27
4Evergreen4 <everett@mcjohnso.com>
2016-01-11 17:06:14 -0500

    Cleanup

--- README.txt
+++ README.txt
@@ -1,30 +1,20 @@
- _____             _       _____             
-|_   _|           | |     /  __ \            
-  | |_ __ __ _ ___| |__   | /  \/ __ _ _ __  
-  | | '__/ _` / __| '_ \  | |    / _` | '_ \ 
+ _____             _       _____
+|_   _|           | |     /  __ \
+  | |_ __ __ _ ___| |__   | /  \/ __ _ _ __
+  | | '__/ _` / __| '_ \  | |    / _` | '_ \
   | | | | (_| \__ \ | | | | \__/\ (_| | | | |
   \_/_|  \__,_|___/_| |_|  \____/\__,_|_| |_|
-                                             
-                                             
 
-
-This mod adds a wooden trash can, and a dumpster to the game.  Right click it, put in your trash, and click the empty trash button.   
+This mod adds a wooden trash can, and a dumpster to the game.  Right click it, put in your trash, and click the empty trash button.
 You can also throw things in the wooden trash can by pressing "q" or throwing them out of your inventory.
 
+Version: 0.1.0
+License: MIT (see LICENSE.txt)
 
-Dependencies:  Default
-
-
-
-License:  CC BY-SA 3.0
-
-This work is licensed under a Creative Commons Attribution 3.0 Unported License. 
-More info at http://creativecommons.org/licenses/by/3.0/
-
-
+Dependencies:
+default (found in minetest_game)
 
 Crafting:
-
 
 Wooden trash can:
 w = wood planks x = nothing
@@ -35,7 +25,6 @@ w|x|w
 -----
 w|w|w
 
-
 Dumpster:
 i = iron ingot c = coal block g = dark green dye
 
@@ -45,18 +34,13 @@ i|g|i
 -----
 i|i|i
 
-
 Contributers:
 
-
 Zeg9:
-
 Made it so you can throw stuff in the trash can (by pressing "q")
 
-
 Mossmanikin:
-
-Made the nodeboxes for the dumpster, the textures for the wooden trash can, and the texture for the dumpster node.  
-(with some editing by me)
+Made the nodeboxes for the dumpster, the textures for the wooden trash can, and the texture for the dumpster node.
+(with some editing by me). (old)
 
 
--- init.lua
+++ init.lua
@@ -1,9 +1,12 @@
---dofiles
+-- Include dumpster
 dofile(minetest.get_modpath("trash_can").."/dumpster.lua")
 
 
---Node Registration
+--
+-- Node Registration
+--
 
+-- Normal Trash Can
 minetest.register_node("trash_can:trash_can_wooden",{
 	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
 	tiles = {"trash_can_wooden_top.png", "trash_can_wooden_top.png", "trash_can_wooden.png"},
@@ -16,52 +19,56 @@ minetest.register_node("trash_can:trash_can_wooden",{
 			{-0.375000,-0.500000,0.312500,0.375000,0.500000,0.375000},
 			{0.312500,-0.500000,-0.375000,0.375000,0.500000,0.375000},
 			{-0.375000,-0.500000,-0.375000,0.375000,0.500000,-0.312500},
-                        {-0.375000,-0.500000,-0.375000,-0.312500,0.500000,0.375000},
-                        {-0.312500,-0.500000,-0.312500,0.312500,-0.437500,0.312500},
-                }
-        },
-        on_construct = function(pos)
+			{-0.375000,-0.500000,-0.375000,-0.312500,0.500000,0.375000},
+			{-0.312500,-0.500000,-0.312500,0.312500,-0.437500,0.312500},
+		}
+	},
+	on_construct = function(pos)
 		local meta = minetest.env:get_meta(pos)
-			meta:set_string("formspec",
-				"size[8,9]"..
-				"button[0,0;2,1;empty;Empty Trash]"..
-                                "list[current_name;main;3,1;2,3;]"..
-                                "list[current_player;main;0,5;8,4;]")
-                meta:set_string("infotext", "Trash Can")
+		meta:set_string("formspec",
+			"size[8,9]"..
+			"button[0,0;2,1;empty;Empty Trash]"..
+			"list[current_name;main;3,1;2,3;]"..
+			"list[current_player;main;0,5;8,4;]"
+		)
+		meta:set_string("infotext", "Trash Can")
 		local inv = meta:get_inventory()
 		inv:set_size("main", 8*4)
 	end,
-        can_dig = function(pos,player)
+	can_dig = function(pos,player)
 		local meta = minetest.env:get_meta(pos);
 		local inv = meta:get_inventory()
-                return inv:is_empty("main")
-        end,
-        on_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
+				return inv:is_empty("main")
+		end,
+		on_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
 		minetest.log("action", player:get_player_name()..
 			" moves stuff in trash can at "..minetest.pos_to_string(pos))
 	end,
-        on_metadata_inventory_put = function(pos, listname, index, stack, player)
+	on_metadata_inventory_put = function(pos, listname, index, stack, player)
 		minetest.log("action", player:get_player_name()..
 			" moves stuff to trash can at "..minetest.pos_to_string(pos))
 	end,
-        on_metadata_inventory_take = function(pos, listname, index, stack, player)
+	on_metadata_inventory_take = function(pos, listname, index, stack, player)
 		minetest.log("action", player:get_player_name()..
 			" takes stuff from trash can at "..minetest.pos_to_string(pos))
-        end,
+	end,
 	on_receive_fields = function(pos, formname, fields, sender)
 		if fields.empty then
-		local meta = minetest.get_meta(pos)
+			local meta = minetest.get_meta(pos)
 			local inv = meta:get_inventory()
 			for i = 1, inv:get_size("main") do
 				inv:set_stack("main", i, nil)
 			end
 			minetest.sound_play("trash", {to_player=sender:get_player_name(), gain = 1.0})
 		end
-        end,
+	end,
 })
 
---Crafting
+--
+-- Crafting
+--
 
+-- Normal Trash Can
 minetest.register_craft({
 	output = 'trash_can:trash_can_wooden',
 	recipe = {
@@ -71,32 +78,16 @@ minetest.register_craft({
 	}
 })
 
+--
+-- Misc
+--
 
---Throw In Trash Can Code
-
+-- Remove any items thrown in trash can.
 local old_on_step = minetest.registered_entities["__builtin:item"].on_step
 minetest.registered_entities["__builtin:item"].on_step = function(self, dtime)
-    if minetest.env:get_node(self.object:getpos()).name == "trash_can:trash_can_wooden" then
-        self.object:remove()
-        return
-    end
-    old_on_step(self, dtime)
+	if minetest.get_node(self.object:getpos()).name == "trash_can:trash_can_wooden" then
+		self.object:remove()
+		return
+	end
+	old_on_step(self, dtime)
 end
-
---Unused stuff
---minetest.register_node("trash_can:trash_can_full",{
---	groups = {choppy=2},
---	tiles = {"default_wood.png"},
---	drawtype="nodebox",
---	paramtype = "light",
---	node_box = {
---		type = "fixed",
---		fixed = {
---			{-0.375000,-0.500000,0.312500,0.375000,0.500000,0.375000}, --NodeBox 1
---			{0.312500,-0.500000,-0.375000,0.375000,0.500000,0.375000}, --NodeBox 2
---			{-0.375000,-0.500000,-0.375000,0.375000,0.500000,-0.312500}, --NodeBox 3
---			{-0.375000,-0.500000,-0.375000,-0.312500,0.500000,0.375000}, --NodeBox 4
---			{-0.312500,-0.500000,-0.312500,0.312500,0.375000,0.312500}, --NodeBox 5
---		}
---	}
---})
